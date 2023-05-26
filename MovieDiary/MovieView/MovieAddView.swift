2d9ed7a2854ec90b0a46744e9c056b26b1158106
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-27 02:42:39 +0900

    feat. add MovieAddView
    - MovieAddView 대략적인 형태만 작업

---
+++ MovieDiary/MovieView/MovieAddView.swift
@@ -0,0 +1,62 @@
+//
+//  MovieAddView.swift
+//  MovieDiary
+//
+//  Created by David Goggins on 2023/05/27.
+//
+
+import SwiftUI
+
+struct MovieAddView: View {
+    var body: some View {
+        VStack {
+            Image("가디언즈")
+                .resizable()
+                .foregroundColor(.accentColor)
+                .frame(width: 420*0.3, height: 694*0.3)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Title:")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Text("guardians of the galaxy vol 3")
+                        .font(.title3)
+                }
+                HStack() {
+                    Text("Date:")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Text("2023.05.27")
+                        .font(.title3)
+                }
+                HStack() {
+                    Text("Where:")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Text("CGV 포항")
+                        .font(.title3)
+                }
+                HStack() {
+                    Text("With Whom")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Text("환, 가락, 동영이")
+                        .font(.title3)
+                }
+                HStack() {
+                    Text("Review")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                    Text("이 영화의 내용은 정말 재미가 있네요 도대체누가 만든거죠 정말 너무 재미있어서 너무 신아요 너무 힘들어요 다이어티 다이어트 힘들어요")
+                        .font(.title3)
+                }
+            }
+        }
+    }
+}
+
+struct MovieAddView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieAddView()
+    }
+}
