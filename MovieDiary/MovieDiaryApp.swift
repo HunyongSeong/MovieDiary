d020b863954ae9adf40b23b4cb6021909c4397b4
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-27 01:49:42 +0900

    Initial Commit

---
+++ MovieDiary/MovieDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDiaryApp.swift
+//  MovieDiary
+//
+//  Created by David Goggins on 2023/05/27.
+//
+
+import SwiftUI
+
+@main
+struct MovieDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
