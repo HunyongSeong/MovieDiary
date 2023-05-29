e7f7493325ad1d14afd0d988429da28440794c43
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-29 18:59:59 +0900

    feat. 메모 기능적인 면은 완성

--- MovieDiary/MovieView/MovieStoryMemo.swift
+++ MovieDiary/MovieView/MovieStoryMemo.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 // 1. 모델 생성
-struct MovieStoryMemo: Identifiable { //MovieStoryMemo 구조체는 Identifiable 프로토콜을 채택하고 있습니다
+struct MovieStoryMemo: Identifiable {
     let id = UUID()
     var title: String
     var content: String
@@ -16,17 +16,16 @@ struct MovieStoryMemo: Identifiable { //MovieStoryMemo 구조체는 Identifiable
 
 // 2. 리스트 뷰 생성
 struct MovieStoryMemoView: View {
-    @State var movieStoryMemos: [MovieStoryMemo] = [] //movieStoryMemos 배열을 사용하여 영화 메모를 보여주는 리스트 뷰를 구성할 수 있습니다
-    @State var movieStoryMemo: MovieStoryMemo = MovieStoryMemo(title: "", content: "")
-    // 즉, 위의 코드는 MovieStoryMemo 구조체를 사용하여 영화 메모를 저장하고, movieStoryMemos 배열을 사용하여 영화 메모를 보여주는 리스트 뷰를 생성하는 SwiftUI 코드
+    @State private var movieStoryMemos: [MovieStoryMemo] = []
+    @State private var movieStoryMemo: MovieStoryMemo = MovieStoryMemo(title: "", content: "")
     
     var body: some View {
         NavigationView {
-            List { // List가 없다면, 화면에 그려지지 않을까? -> 그냥 포문으로만 그려진다. 하지만 리스트를 사용하면 리스트 안에 그려주게 됨.
-                ForEach($movieStoryMemos) { $memo in //movieStoryMemos 각 배열의 요소 하나하나를 memo라 하겠다.
-                    NavigationLink(destination: MovieMemoDetailView(memo: $memo)) { // 차례대로 클릭시 해당 디테일 뷰로 이동
+            List {
+                ForEach($movieStoryMemos) { $memo in // 여기서는 $를 붙여주는 것이 핵심
+                    NavigationLink(destination: MovieMemoDetailView(memo: $memo)) {
                         VStack(alignment: .leading) {
-                            Text(memo.title) // 각각 해당 디테일뷰의 제목을 보여줌
+                            Text(memo.title)
                                 .font(.headline)
                             Text(memo.content)
                                 .font(.subheadline)
@@ -46,15 +45,13 @@ struct MovieStoryMemoView: View {
 }
 
 // 3. 메모 클릭시, 상세 정보를 표시하는 뷰를 생성
-struct MovieMemoDetailView: View { //movieStoryMemo에는 2. 에서 movieStoryMemos 배열의 요소 하나하나가 들어가게 된다.
+struct MovieMemoDetailView: View {
     @Binding var memo: MovieStoryMemo
-//    @Binding var movieStoryMemos: [MovieStoryMemo]
-    @Environment(\.presentationMode) var presentationMode // 현재 뷰의 표시 모드를 나타내는 환경 변수입니다. 이를 사용하여 뷰를 닫거나 이동하기 위해 필요한 기능을 수행할 수 있습니다.
+    @Environment(\.presentationMode) var presentationMode
 
-    
     var body: some View {
         VStack{
-            TextEditor(text: self.$memo.title) // 이렇게 요소 하나하나를 화면에 보여줌과 동시에, 추가적인 채팅으로 바꾸는 것 또한 가능.
+            TextField("Title", text: self.$memo.title)
                 .font(.headline)
                 .padding(.vertical, 8)
             Divider()
@@ -62,22 +59,15 @@ struct MovieMemoDetailView: View { //movieStoryMemo에는 2. 에서 movieStoryMe
                 .font(.subheadline)
         }
         .padding()
-//        .navigationBarItems(trailing: Button("Save") {
-//            // 현재 자리에 있는 것을 빼고 현재 자리에 추가해 줘야 함. -> 내 자리를 알아야 함.내 자리를 지우고, 내 자리에 추가해주기.
-////            movieStoryMemos.removeLast() // 이 한줄이 추가 됨으로써 업데이트가 가능해짐. 추가 안될 시, 기존에 써놓았던 양식이 복사됨
-//            presentationMode.wrappedValue.dismiss()
-//        }
-//            .disabled(self.memo.title.isEmpty || self.memo.content.isEmpty)
-//        //조건 중 하나라도 true이면, 즉 제목이나 내용 중 하나라도 비어있는 경우, .disabled(true)가 적용되어 버튼이 비활성화됩니다.
-//        )
     }
 }
 
 // 4. 추가/수정 뷰 생성
 struct MovieMemoEditView: View {
     @Binding var movieStoryMemos: [MovieStoryMemo]
-    @State var movieStoryMemo: MovieStoryMemo = MovieStoryMemo(title: "", content: "") // 메모장을 새로 만들 때, 해당 변수는 항상 값을 초기화하고 값이 넣어지고 savee되면서 movieStoryMemos배열에 값이 넣어지면서 끝이난다.
-    @Environment(\.presentationMode) var presentationMode // 현재 뷰의 표시 모드를 나타내는 환경 변수입니다. 이를 사용하여 뷰를 닫거나 이동하기 위해 필요한 기능을 수행할 수 있습니다.
+    @State var movieStoryMemo: MovieStoryMemo = MovieStoryMemo(title: "", content: "")
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         VStack {
             TextField("Titile", text: $movieStoryMemo.title)
